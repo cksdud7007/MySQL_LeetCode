9faea8894b9b8ab48d3b886e065d2ebe7c2dc23b
cksdud7007 <84171662+cksdud7007@users.noreply.github.com>
2023-06-13 22:44:03 +0900

    Time: 1154 ms (18.48%), Space: 0B (100.00%) - LeetHub

---
+++ 1393-capital-gain-loss/1393-capital-gain-loss.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+with buy as (select stock_name, sum(-price) as price
+                from Stocks
+             where operation = 'Buy'
+             group by stock_name),
+     sell as (select stock_name, sum(price) as price
+                from Stocks
+             where operation = 'Sell'
+             group by stock_name)
+             
+select buy.stock_name, buy.price + sell.price as capital_gain_loss
+    from buy
+join sell
+on buy.stock_name = sell.stock_name
+group by buy.stock_name
