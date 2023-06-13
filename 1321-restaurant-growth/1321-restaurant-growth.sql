829fbe95fdad49adee29fcd204295a73894d097b
cksdud7007 <84171662+cksdud7007@users.noreply.github.com>
2023-06-14 08:09:40 +0900

    Time: 770 ms (43.12%), Space: 0B (100.00%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+# select C.visited_on, sum(C.amount) as amount, avg(C.amount) as average_amount
+#     from Customer as C
+# left join Customer as Cu
+# on datediff(Cu.visited_on,C.visited_on) <= 7
+# group by C.visited_on
+# having count(C.visited_on) = 7
+
+SELECT date_add(C.visited_on,interval 6 day) as visited_on, sum(Cu.amount) as amount, round(sum(Cu.amount)/7,2) as average_amount
+    FROM (SELECT DISTINCT visited_on FROM Customer) AS C
+JOIN Customer AS Cu ON Cu.visited_on >= C.visited_on and datediff(Cu.visited_on, C.visited_on) < 7
+group by C.visited_on
+having count(distinct Cu.visited_on) = 7
+order by C.visited_on
+
