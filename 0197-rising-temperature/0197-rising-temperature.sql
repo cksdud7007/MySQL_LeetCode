86c4a18d0a279ed2c6d1d9db79d623ec3b87812e
cksdud7007 <84171662+cksdud7007@users.noreply.github.com>
2023-06-15 00:59:14 +0900

    Time: 993 ms (30.68%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -2,5 +2,4 @@
 select W.id
     from Weather as W
 join (select * from Weather) as We
-on W.temperature > We.temperature
-where datediff(W.recordDate, We.recordDate) = 1
+on W.temperature > We.temperature and datediff(W.recordDate, We.recordDate) = 1
